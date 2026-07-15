a0d6a76b200b3f507b1a9132358ac1ee3683cfa1
herbertmoroni <herbertmoroni@hotmail.com>
2026-07-14 20:50:33 -0600

    improve colors

--- visualize.R
+++ visualize.R
@@ -3,16 +3,13 @@ library(visNetwork)
 library(htmltools)
 
 plot_network <- function(g, bridges, path = "network.html") {
-  palette <- c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
-               "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf")
-  community_colors <- palette[((V(g)$community - 1) %% length(palette)) + 1]
   is_bridge <- V(g)$name %in% bridges$number
 
   nodes <- data.frame(
     id = V(g)$name,
     label = V(g)$name,
     group = paste0("Community ", V(g)$community),
-    color = ifelse(is_bridge, "red", community_colors),
+    color = ifelse(is_bridge, "red", "#1f77b4"),
     shape = ifelse(is_bridge, "triangle", "dot"),
     value = degree(g),
     title = paste0(
